ee67e7bf3a2bedc8bb98695a1dd4f771c7d666c8
Xandromus <xanderrapstine@gmail.com>
2018-06-19 13:11:17 -0500

    started working on colors

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import fortyacreslogo from "../fortyacreslogo.svg";
 import '../styles/Main.css';
 
 export default class Main extends Component {
   render() {
     return (
-      <div className="main">
-      Hi
-        <div className="middle">
-        Hi
+      <div className="main mt-5">
+        <div className="middle mt-5">
+        <img id="brand-logo" className="mx-auto mt-5" src={fortyacreslogo} />
         </div>
       </div>
     )
