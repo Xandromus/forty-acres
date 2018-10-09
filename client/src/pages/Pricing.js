e39c24e29b25bb7546a35648dc31a76b45602d45
Xandromus <xanderrapstine@gmail.com>
2018-10-09 12:03:42 -0500

    constricting input and making page numbers always round to even

--- client/src/pages/Pricing.js
+++ client/src/pages/Pricing.js
@@ -20,25 +20,24 @@ export default class Pricing extends Component {
     let books = this.state.books;
     let rate = 0;
 
-    console.log(pages, books, rate, total);
-
     if (pages > 249) {
       rate = 0.06;
-      console.log(rate);
     } else if (pages > 100 && pages < 250) {
       rate = 0.08;
-      console.log(rate);
     } else if (pages < 101) {
       rate = 0.1;
-      console.log(rate);
     } else {
       rate = 0;
-      console.log(rate);
     }
 
-    total = (pages * rate + 2.5) * books;
-    total = total.toFixed(2);
+    if (pages % 2 !== 0) {
+      pages = parseInt(pages, 10) + 1;
+    }
 
+    if (pages > 0) {
+      total = (pages * rate + 2.5) * books;
+    }
+    total = total.toFixed(2);
     this.setState({
       total: total
     })
@@ -53,58 +52,18 @@ export default class Pricing extends Component {
       this.handlePriceChange();
     });
 
-
-
   };
-
-  // updatePrice = () => {
-  //   console.log(this);
-  //   this.handleInputChange()
-  //     .then(price => {
-  //       this.handlePriceChange();
-  //     })
-  // }
-
-
-
-  // handleBooksChange = event => {
-  //   const { name, value } = event.target;
-
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // }
-
-  // handleFormSubmit(event) {
-  //   event.preventDefault();
-  //   let pages = this.state.pages;
-  //   let books = this.state.books;
-  //   let rate = this.state.rate;
-
-  //   if (pages > 249) {
-  //     this.setState({ rate: 0.06 });
-  //   } else if (pages > 100 && pages < 250) {
-  //     this.setState({ rate: 0.08 });
-  //   } else if (pages < 101) {
-  //     this.setState({ rate: 0.1 });
-  //   } else {
-  //     this.setState({ rate: 0.00 });
-  //   }
-
-  //   total = (pages * rate + 2.5) * books;
-
-  // }
 
   render() {
     return (
       <div className="pricing">
         <div className="middle my-3 container">
           <h2>Pricing Calculator</h2>
-          <form>
+          <form className="calculator">
             <div>
               <label htmlFor="pages"># of pages</label>
               <Input
-                type="text"
+                type="number"
                 className="form-control d-inline-block"
                 name="pages"
                 id="pages"
@@ -115,7 +74,7 @@ export default class Pricing extends Component {
             <div>
               <label htmlFor="pages"># of books</label>
               <Input
-                type="text"
+                type="number"
                 className="form-control d-inline-block"
                 name="books"
                 id="books"
@@ -127,16 +86,6 @@ export default class Pricing extends Component {
               <p>Estimate: ${this.state.total}</p>
 
             </div>
-            {/* <div>
-              <label htmlFor="total">Estimate: $</label>
-              <Input type="text" className="form-control d-inline-block" name="total" id="total" value={total} />
-            </div>
-            <div>
-              <Input
-                type="submit"
-                className="btn btn-primary"
-                value="Estimate Price" />
-            </div> */}
           </form>
         </div>
       </div>
