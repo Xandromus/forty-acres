1fa9e6ba90bf87ea10507d823caed780e2f32a0f
Xandromus <xanderrapstine@gmail.com>
2018-10-14 17:22:55 -0500

    having trouble getting api route to work

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,10 +1,104 @@
 import React, { Component } from 'react';
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Input } from 'reactstrap';
+import "../styles/Admin.css";
 
 export default class Admin extends Component {
+
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    synopsis: ""
+  };
+
+  componentDidMount() {
+    this.loadBooks();
+  }
+
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.title && this.state.author) {
+      API.saveBook({
+        title: this.state.title,
+        author: this.state.author,
+        synopsis: this.state.synopsis
+      })
+        .then(res => this.loadBooks())
+        .catch(err => console.log(err));
+    }
+  };
+
   render() {
     return (
       <div className="admin">
-        { this.props.children }
+      <div className="middle my-3 container">
+      <div className="row">
+        <div className="div-md-6">
+          <form>
+            <Input
+              value={this.state.title}
+              onChange={this.handleInputChange}
+              name="title"
+              placeholder="Title (required)"
+            />
+            <Input
+              value={this.state.author}
+              onChange={this.handleInputChange}
+              name="author"
+              placeholder="Author (required)"
+            />
+            <Input
+              type="textarea"
+              value={this.state.synopsis}
+              onChange={this.handleInputChange}
+              name="synopsis"
+              placeholder="Synopsis (Optional)"
+            />
+            <button
+              disabled={!(this.state.author && this.state.title)}
+              onClick={this.handleFormSubmit}
+            >
+              Submit Book
+              </button>
+          </form>
+        </div>
+        <div className="col-md-6 col-sm-12">
+            {this.state.books.length ? (
+              <ul>
+                {this.state.books.map(book => (
+                  <li key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </Link>
+                    <button onClick={() => this.deleteBook(book._id)} />
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </div>
+          </div>
+          </div>
       </div>
     )
   }
